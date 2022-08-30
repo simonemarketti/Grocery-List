d7fe84d4a1a6af78b60f461ba9c07a122bc2cbd8
simonemarketti <simomarketti@icloud.com>
2022-08-30 13:14:15 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
+// Query
 const addForm = document.querySelector('.add-form');
 const search = document.querySelector('.search input');
 const productsList = document.querySelector('.products-list');
 
-// ADD AND DELETE TODOS 
-
 // create function to add product
 const refreshList = product => {
-  const newProduct = `
+    const newProduct = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <span>${product}</span>
       <i class="fas fa-cart-arrow-down delete"></i>
     </li>
   `;
 
-  // create products counter
-  let counter = productsList.childElementCount + 1;
-  // and max products logic
-  if (counter < 8) {
-    productsList.innerHTML += newProduct;
-  } else {
-    productsList.innerHTML += newProduct;
-    addForm.add.setAttribute('disabled', '');
-  }
+    // create products counter
+    let counter = productsList.childElementCount + 1;
+    // and max products logic
+    if (counter < 8) {
+        productsList.innerHTML += newProduct;
+    } else {
+        productsList.innerHTML += newProduct;
+        addForm.add.setAttribute('disabled', '');
+    }
 }
 
 // add products event 
 addForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const product = addForm.add.value.trim();
-  if (product.length) {
-    refreshList(product);
-    addForm.reset();
-  }
+    e.preventDefault();
+    const product = addForm.add.value.trim();
+    if (product.length) {
+        refreshList(product);
+        addForm.reset();
+    }
 });
 
 // delete products event (applied to the father "ul" using delegation)
 productsList.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
-  }
-  addForm.add.removeAttribute('disabled');
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+    addForm.add.removeAttribute('disabled');
 });
 
 // create function to search products
 const filterProducts = word => {
-  // add filtered class
-  Array.from(productsList.children)
-    .filter(product => !product.textContent.toLowerCase().includes(word))
-    .forEach(product => product.classList.add('filtered'));
+    // add filtered class
+    Array.from(productsList.children)
+        .filter(product => !product.textContent.toLowerCase().includes(word))
+        .forEach(product => product.classList.add('filtered'));
 
-  // remove filtered class
-  Array.from(productsList.children)
-    .filter(product => product.textContent.toLowerCase().includes(word))
-    .forEach(product => product.classList.remove('filtered'));
+    // remove filtered class
+    Array.from(productsList.children)
+        .filter(product => product.textContent.toLowerCase().includes(word))
+        .forEach(product => product.classList.remove('filtered'));
 
 };
 
 // filter products event
 search.addEventListener('keyup', () => {
-  const term = search.value.trim().toLowerCase();
-  filterProducts(term);
+    const term = search.value.trim().toLowerCase();
+    filterProducts(term);
 });
