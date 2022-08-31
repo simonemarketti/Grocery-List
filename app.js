48d37808a9ca76b98873e1688335a7560448fe97
simonemarketti <simomarketti@icloud.com>
2022-08-31 20:01:25 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+// DOM
+
 // Query
 const addForm = document.querySelector('.add-form');
 const search = document.querySelector('.search input');
 const productsList = document.querySelector('.products-list');
 
-// create function to add product
+// FUNCTIONS
+
+// Create function to add product
 const refreshList = product => {
     const newProduct = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -11,7 +15,6 @@ const refreshList = product => {
       <i class="fas fa-cart-arrow-down delete"></i>
     </li>
   `;
-
     // create products counter
     let counter = productsList.childElementCount + 1;
     // and max products logic
@@ -23,25 +26,7 @@ const refreshList = product => {
     }
 }
 
-// add products event 
-addForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const product = addForm.add.value.trim();
-    if (product.length) {
-        refreshList(product);
-        addForm.reset();
-    }
-});
-
-// delete products event (applied to the father "ul" using delegation)
-productsList.addEventListener('click', e => {
-    if (e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
-    }
-    addForm.add.removeAttribute('disabled');
-});
-
-// create function to search products
+// Create function to search products
 const filterProducts = word => {
     // add filtered class
     Array.from(productsList.children)
@@ -52,10 +37,29 @@ const filterProducts = word => {
     Array.from(productsList.children)
         .filter(product => product.textContent.toLowerCase().includes(word))
         .forEach(product => product.classList.remove('filtered'));
-
 };
 
-// filter products event
+// EVENTS
+
+// Add products event
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const product = addForm.add.value.trim();
+    if (product.length) {
+        refreshList(product);
+        addForm.reset();
+    }
+});
+
+// Delete products event (applied to the father "ul" using delegation)
+productsList.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+    addForm.add.removeAttribute('disabled');
+});
+
+// Filter products event
 search.addEventListener('keyup', () => {
     const term = search.value.trim().toLowerCase();
     filterProducts(term);
